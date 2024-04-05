b91be8619a13afb445c55f28f273bab153d21d37
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2024-04-05 11:15:32 +0200

    Greate Site Made

---
+++ SommerhusHjemmeside/Pages/UserSite/Greate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SommerhusHjemmeside.Pages.UserSite
+{
+    public class GreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
