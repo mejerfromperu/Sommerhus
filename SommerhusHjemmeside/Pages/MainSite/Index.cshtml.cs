63030e13c98a21197af8324c4b2e20789227cc07
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2024-04-05 09:41:36 +0200

    yessir

---
+++ SommerhusHjemmeside/Pages/MainSite/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SommerhusHjemmeside.Pages.MainSite
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
