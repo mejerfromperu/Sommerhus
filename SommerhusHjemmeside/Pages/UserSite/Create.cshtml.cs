418e76a3b0d5b4d6a24585dc9641f70b4c67a331
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-04-09 09:52:56 +0200

    create

--- SommerhusHjemmeside/Pages/UserSite/Create.cshtml.cs
+++ SommerhusHjemmeside/Pages/UserSite/Create.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sommerhus.Repository07;
 
 namespace SommerhusHjemmeside.Pages.UserSite
 {
     public class CreateModel : PageModel
     {
+
+        private IUserRepostiroy _repo;
+
+
         public void OnGet()
         {
         }
