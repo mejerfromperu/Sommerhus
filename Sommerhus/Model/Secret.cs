a2f20d0be36d583e0ae9aafc1285fcb8bd01d808
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-04-09 13:35:56 +0200

    con string

---
+++ Sommerhus/Model/Secret.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sommerhus.Model
+{
+    public class Secret
+    {
+
+        public static string GetConnectionString
+
+        {
+
+            get { return "Data Source=mssql16.unoeuro.com;User ID=isakgm_dk;Password=f2t9wHmFRDenbEA53ghp;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"; }
+
+        }
+    }
+}
