79212082ddb06d732ab42cdfe1b1a9ef198b7e00
Isakgm <Isakgm@gmail.com>
2024-04-09 09:44:28 +0200

    Fix

--- SommerhusHjemmeside/Pages/UserSite/Create.cshtml.cs
+++ SommerhusHjemmeside/Pages/UserSite/Create.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SommerhusHjemmeside.Pages.UserSite
 {
-    public class GreateModel : PageModel
+    public class CreateModel : PageModel
     {
         public void OnGet()
         {
