37d830fc3489c18164e0bdb284bd51ec0cbb51e9
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-04-19 11:31:00 +0200

    test

---
+++ SommerHouseTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SommerHouseTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ SommerhusUserTests/Repository07/UserRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sommerhus.Model;
+using Sommerhus.Repository07;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sommerhus.Repository07.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        private UserRepository _userRepository;
+
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+
+            User userToAdd = new User();
+
+            _userRepository.Add(userToAdd);
+
+
+            Assert.IsNull(_userRepository);
+        }
+
+        //[TestMethod()]
+        //public void DeleteTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetAllTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetByIdTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void UpdateTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetByEmailAndPasswordTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void SearchTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
